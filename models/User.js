3385908528a0e3c446b45af4237b1bf39c63eaa8
Jing Li <lijing900913@gmail.com>
2022-01-12 17:53:32 -0500

    Add outline for the project

---
+++ models/User.js
@@ -0,0 +1,45 @@
+// **User**:
+
+const { Schema } = require("mongoose");
+
+// * `username`
+//   * String
+//   * Unique
+//   * Required
+//   * Trimmed
+
+// * `email`
+//   * String
+//   * Required
+//   * Unique
+//   * Must match a valid email address (look into Mongoose's matching validation)
+
+// * `thoughts`- an array, need to populate all the thougths in userControllers
+//   * Array of `_id` values referencing the `Thought` model
+thoughts: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'thought'
+    }
+]
+
+// * `friends`
+//   * Array of `_id` values referencing the `User` model (self-reference)
+friends: [
+    {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }
+]
+// **Schema Settings**:
+// Create a virtual called `friendCount` that retrieves the length of the user's `friends` array field on query.
+userSchema
+    .virtual('friendCount')
+    .get(function() {
+        return this.friends.length;
+    });
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
